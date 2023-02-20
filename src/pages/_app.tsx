d8948c31ba9addb5e702a9dcf2671b9dd2bc6f25
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-02-20 20:02:31 -0300

    feat: component prompt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Source_Code_Pro } from "@next/font/google";
+
+const sourceCodePro = Source_Code_Pro({ subsets: ["latin"], weight: "300",  variable: "--source-font" });
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<div className={sourceCodePro.className}>
+			<Component {...pageProps} />
+		</div>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
+import Prompt from "@/components/Prompt";
 import styles from "@/styles/Home.module.css";
-import { Inter } from "@next/font/google";
 import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 	return (
@@ -14,7 +12,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className={styles.main}>
-				<h1>Ola</h1>
+				<Prompt />
 			</main>
 		</>
 	);
