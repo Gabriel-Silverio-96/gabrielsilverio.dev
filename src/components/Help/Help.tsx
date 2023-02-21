8261c14ecf1f2801f3881858945e5dbc388af34f
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-02-21 05:57:48 -0300

    feat: command option help

---
+++ src/components/Help/Help.tsx
@@ -0,0 +1,34 @@
+import styles from "./styles/Help.module.css";
+
+const Help = () => (
+	<section className={styles.help}>
+		<h4>type: gs "command"</h4>
+
+		<div className={styles.commands}>
+			<h4>Comands</h4>
+
+			<table>
+				<tbody>
+					<tr>
+						<td>help</td>
+						<td>View all options</td>
+					</tr>
+					<tr>
+						<td>project</td>
+						<td>See what project I'm currently working on</td>					
+					</tr>
+					<tr>
+						<td>about</td>
+						<td>With this option you can get to know me a little more</td>					
+					</tr>
+					<tr>
+						<td>contact</td>
+						<td>More information about the media</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</section>
+);
+
+export default Help;
--- src/utils/comands/comands.tsx
+++ src/utils/comands/comands.tsx
@@ -1,10 +1,11 @@
+import Help from "@/components/Help";
 import NotRecognizedCommand from "@/components/NotRecognizedCommand";
 import Prompt from "@/components/Prompt";
 import { Command, ICommandsOptions } from "./types/Command";
 
 const commands = (command: Command) => {
 	const commandsOptions: ICommandsOptions = {
-		gs_help: [() => <h1 style={{color: "wheat"}}>Help</h1>, Prompt]
+		gs_help: [Help, Prompt],
 	};
 
 	const notRecognizedCommand = [NotRecognizedCommand, Prompt];
