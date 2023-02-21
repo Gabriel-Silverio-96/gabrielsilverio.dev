c2db0235c8addbb98c024438b27af8c1a3f244d5
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-02-21 04:55:15 -0300

    feat: message not a recognized command

---
+++ src/components/NotRecognizedCommand/NotRecognizedCommand.tsx
@@ -0,0 +1,7 @@
+import styles from "./styles/NotRecognizedCommand.module.css";
+
+const NotRecognizedCommand = () => (
+	<h4 className={styles.message}>Not a recognized command</h4>
+);
+
+export default NotRecognizedCommand;
