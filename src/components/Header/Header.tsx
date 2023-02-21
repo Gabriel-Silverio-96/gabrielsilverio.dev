9e3d9c7428d954651b60d03aa9b6df8268081d90
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-02-21 13:14:18 -0300

    feat: header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles/Header.module.css";
+
+const Header = () => (
+	<header className={styles.header}>
+		<a href="https://github.com/Gabriel-Silverio-96" target="_blank" rel="noreferrer">
+			<h4>Github</h4>
+		</a>
+	</header>
+);
+
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Source_Code_Pro } from "@next/font/google";
+import Header from "@/components/Header";
 
 const sourceCodePro = Source_Code_Pro({ subsets: ["latin"], weight: "300",  variable: "--source-font" });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<div className={sourceCodePro.className}>
+		<div className={sourceCodePro.className}>			
+			<Header />
 			<Component {...pageProps} />
 		</div>
 	);
