94ca6da7b5f3ef3d5e35aa6b8141514fb6f0230c
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-10-23 20:06:37 -0300

    feat: updated ascii art

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,23 +1,21 @@
-import Image from "next/image";
 import styles from "./styles/Project.module.css";
+import TaskYupArt from "@/components/TaskYupArt";
 
 const Project = () => (
 	<div className={styles.project}>
-		<Image
-			src="/logo-taskyup-ascii.png"
-			alt="Logo TaskYup ascii"
-			width={964}
-			height={237}            
-		/>
+		<TaskYupArt />
 		<p>
 			I am currently working on a personal project called TaskYup, which
 			is currently a task organizer in a quick and easy format. This
-			project is evolution other project that build in 2021 called mynotes.
-			I build project using react on the frontend and a node api on the
-			backend.
+			project is evolution other project that build in 2021 called
+			mynotes. I build project using react on the frontend and a node api
+			on the backend.
 		</p>
-		<a href="https://taskyup.com/auth/signup" target="_blank" rel="noreferrer">
-            See more
+		<a
+			href="https://taskyup.com/auth/signup"
+			target="_blank"
+			rel="noreferrer">
+			See more
 		</a>
 	</div>
 );
